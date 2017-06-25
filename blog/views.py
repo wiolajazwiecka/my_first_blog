c8da477012581d4f63e121db4e38ce4eda0138d0
wiola django <wiola@django.pl>
2017-06-25 13:07:18 +0200

    add post on blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
+from .models import Post
 from django.shortcuts import render
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-
+    wszystkie_posty=Post.objects.all()
+    return render(request, 'blog/post_list.html', {"nazwa":wszystkie_posty})
 # Create your views here.
